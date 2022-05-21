27e09e1448a1080d1ea61b3b825b2b2c10912440
Michelle Stone <michellechristine.s91@gmail.com>
2022-05-21 16:52:59 -0400

    Added environment variables to protect database information.

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 // Import the Sequelize constructor from the library.
 const Sequelize = require("sequelize");
 
+require("dotenv").config();
+
 // Create connection to the database, pass in MySQL information for username and password.
-const sequelize = new Sequelize("logistics_db", "username", "password", {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3306,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
 
 module.exports = sequelize;
