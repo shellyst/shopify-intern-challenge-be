3bb3f481df9d3d01300b053cd088003532ba2aad
Michelle Stone <michellechristine.s91@gmail.com>
2022-05-21 19:39:33 -0400

    Added api routes for all and single products.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,2 +1,57 @@
 const router = require("express").Router();
+const { Warehouse, Category } = require("../../models");
 const { Product } = require("../../models/Category");
+
+// Gets all product routes.
+router.get("/", (req, res) => {
+  // Includes Product and Warehouse.
+  Product.findAll({
+    attributes: ["id", "product_name", "price", "stock", "category_id"],
+    include: [
+      {
+        model: Category,
+        attributes: ["id", "category_name"],
+      },
+      {
+        model: Warehouse,
+        attributes: ["warehouse_name"],
+      },
+    ],
+  })
+    .then((productData) => res.json(productData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// Return a single product by id.
+router.get(":/id", (req, res) => {
+  // Includes associated Product and Warehouse.
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ["id", "category_name"],
+      },
+      {
+        model: Warehouse,
+        attributes: ["warehouse_name"],
+      },
+    ],
+  })
+    .then((productData) => {
+      if (!productData) {
+        res.status(404).json({ message: "No product found with that id." });
+        return;
+      }
+      res.json(productData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
