a47b7e1db192707683f4a301121cfd57e908ed38
Michelle Stone <michellechristine.s91@gmail.com>
2022-05-21 21:30:56 -0400

    Created product update route.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Warehouse, Category } = require("../../models");
+const { Warehouse, Category, ProductWarehouse } = require("../../models");
 const { Product } = require("../../models/Category");
 
 // Gets all product routes.
@@ -53,5 +53,76 @@ router.get(":/id", (req, res) => {
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
+    });
+});
+
+// Creates a new product.
+router.post("/", (req, res) => {
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    warehouseIds: req.body.warehouseIds,
+  }).then((product) => {
+    //   If there are product warehouses, we need to create pairings to bulk create in the ProductWarehouse model.
+    if (req.body.warehouseIds.length) {
+      const productWarehouseIdArr = req.body.warehouseIds.map(
+        (warehouse_id) => {
+          return {
+            product_id: product.id,
+            warehouse_id,
+          };
+        }
+      );
+      return ProductWarehouse.bulkCreate(productWarehouseIdArr);
+    }
+    // If no product warehouses, just respond.
+    res.status(200).json(product);
+  });
+});
+
+// Update product.
+router.put("/:id", (req, res) => {
+  // Update product data.
+  Product.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((product) => {
+      //   Find all associated warehouses from ProductWarehouse.
+      return ProductWarehouse.findAll({ where: { product_id: req.params.id } });
+    })
+    .then((productWarehouses) => {
+      //   Get list of current warehouse ids.
+      const productWarehouseIds = productWarehouses.map(
+        ({ warehouse_id }) => warehouse_id
+      );
+      //   Create filtered list of new warehouse_ids.
+      const newProductWarehouses = req.body.warehouseIds
+        .filter((warehouse_id) => !productWarehouseIds.includes(warehouse_id))
+        .map((warehouse_id) => {
+          return {
+            product_id: req.params.id,
+            warehouse_id,
+          };
+        });
+      // Figure out which ones to remove.
+      const productWarehouseToRemove = productWarehouses
+        .filter(
+          ({ warehouse_id }) => !req.body.warehouseIds.includes(warehouse_id)
+        )
+        .map(({ id }) => id);
+
+      // Run both actions.
+      return Promise.all([
+        ProductWarehouse.destroy({ where: { id: productWarehouseToRemove } }),
+        ProductWarehouse.bulkCreate(newProductWarehouses),
+      ]);
+    })
+    .then((updatedProductWarehouses) => res.json(updatedProductWarehouses))
+    .catch((err) => {
+      res.status(400).json(err);
     });
 });
