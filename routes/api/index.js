d3b94233be5f6a27173f78f23a028b8a6a067aec
Michelle Stone <michellechristine.s91@gmail.com>
2022-05-21 22:23:06 -0400

    Set up server file to import routes and turn server on.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const productRoutes = require("./product-routes");
+const categoryRoutes = require("./category-routes");
+const warehouseRoutes = require("./warehouse-routes");
 
 router.use("/products", productRoutes);
+router.use("/categories", categoryRoutes);
+router.use("/warehouses", warehouseRoutes);
 
 module.exports = router;
