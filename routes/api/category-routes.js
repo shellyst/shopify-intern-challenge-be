d7ba252a72d052e0fb1858689c093d02b7bcb4da
Michelle Stone <michellechristine.s91@gmail.com>
2022-05-21 21:59:02 -0400

    Added get one/get all routes for category.

---
+++ routes/api/category-routes.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+const { Product, Category } = require("../../models");
+
+// The `/api/categories` endpoint.
+router.get("/", (req, res) => {
+  // Includes associated Products.
+  Category.findAll({
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((category) => res.json(category))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// Finds one category by its own id value.
+router.get("/:id", (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((category) => {
+      if (!category) {
+        res.status(400).json({ message: "No category found with that id." });
+        return;
+      }
+      res.json(category);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
