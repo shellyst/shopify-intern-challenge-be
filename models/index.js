66ed72c7765eb38b2526f8d905e4915df9a04b3b
Michelle Stone <michellechristine.s91@gmail.com>
2022-05-21 20:13:24 -0400

    Created associations in models index.js

--- models/index.js
+++ models/index.js
@@ -3,4 +3,27 @@ const Category = require("./Category");
 const Warehouse = require("./Warehouse");
 const ProductWarehouse = require("./ProductWarehouse");
 
+// Product belongsTo Category.
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+
+// Categories have many products.
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
+
+// Products belongToMany Warehouses (through ProductWarehouse).
+Product.belongsToMany(Warehouse, {
+  through: ProductWarehouse,
+  foreignKey: "product_id",
+});
+
+// Warehouses belongToMany Products (through ProductWarehouse).
+Warehouse.belongsToMany(Product, {
+  through: ProductWarehouse,
+  foreignKey: "warehouse_id",
+});
+
 module.exports = { Product, Category, Warehouse, ProductWarehouse };
